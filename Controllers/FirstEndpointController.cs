b991b53b2829dd0da04ebda765a34c06e3ae0007
Miłosz Gilga <gilgamilosz451@gmail.com>
2022-03-29 11:34:31 +0200

    initial commit and generate some boilerplate code

---
+++ Controllers/FirstEndpointController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using asp_net_po_schedule_management_server.Utils;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace asp_net_po_schedule_management_server.Controllers
+{
+    [ApiController]
+    [Route(nameof(Configuration.API_PREFIX) + nameof(Configuration.WEATHER_FORECAST_ENDPOINT))]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public Action<string> Get([FromRoute] string message)
+        {
+            return 
+        }
+    }
+}
