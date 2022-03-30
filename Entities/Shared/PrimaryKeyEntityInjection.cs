067ef382313af564db649d18449b3ee4b009bcdc
Miłosz Gilga <gilgamilosz451@gmail.com>
2022-03-30 18:53:47 +0200

    create base entities (for inheritance) and remove unused primary-key column

---
+++ Entities/Shared/PrimaryKeyEntityInjection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace asp_net_po_schedule_management_server.Entities.Shared
+{
+    public abstract class PrimaryKeyEntityInjection
+    {
+        [Key]
+        [Column("primary-key")]
+        public long Id { get; set; }
+        
+        [Column("created-date")]
+        public DateTime CreatedDate { get; set; }
+        
+        [Column("updated-date")]
+        public DateTime UpdatedDate { get; set; }
+    }
+}
