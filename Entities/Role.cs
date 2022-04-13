8872a67d33b98f6930eb2cd19958cb0e72f3ba8b
Miłosz Gilga <gilgamilosz451@gmail.com>
2022-04-13 11:18:26 +0200

    update Person entity configuration and create Role entity (for JWT)

---
+++ Entities/Role.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using asp_net_po_schedule_management_server.Entities.Shared;
+
+
+namespace asp_net_po_schedule_management_server.Entities
+{
+    [Table("role")]
+    public class Role : PrimaryKeyWithClientIdentifierInjection
+    {
+        [Column("name")]
+        [MaxLength(50)]
+        public string Name { get; set; }
+    }
+
+    public enum AvailableRoles
+    {
+        TEACHER,
+        EDITOR,
+        AMINISTRATOR,
+    }
+}
