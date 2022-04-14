1bbf7846ff66ad63a6e07d9c38d78a765d5a9b9d
Miłosz Gilga <gilgamilosz451@gmail.com>
2022-04-15 00:02:56 +0200

    create mapping profile class

---
+++ DbConfig/ApplicationMappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+
+using asp_net_po_schedule_management_server.Entities;
+using asp_net_po_schedule_management_server.Dto.AuthDtos;
+
+
+namespace asp_net_po_schedule_management_server.DbConfig
+{
+    public sealed class ApplicationMappingProfile : Profile
+    {
+        public ApplicationMappingProfile()
+        {
+            CreateMap<RegisterNewUserRequestDto, Person>();
+            CreateMap<Person, RegisterNewUserResponseDto>();
+        }
+    }
+}
