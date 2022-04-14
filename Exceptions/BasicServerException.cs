a8776fecb895a75bdc2d92d1ad54953478da138e
Miłosz Gilga <gilgamilosz451@gmail.com>
2022-04-15 00:09:09 +0200

    create basic server error exception class

---
+++ Exceptions/BasicServerException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+
+
+namespace asp_net_po_schedule_management_server.Exceptions
+{
+    public class BasicServerException : Exception
+    {
+        private HttpStatusCode _httpStatusCode;
+        
+        public BasicServerException(string message, HttpStatusCode httpStatusCode) : base(message)
+        {
+            _httpStatusCode = httpStatusCode;
+        }
+
+        public HttpStatusCode HttpStatusCode => _httpStatusCode;
+    }
+}
