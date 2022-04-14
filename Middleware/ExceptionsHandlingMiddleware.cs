e4adecf010726b1b9f5872ee7ca25d3d27793bb1
Miłosz Gilga <gilgamilosz451@gmail.com>
2022-04-15 00:09:31 +0200

    implement BasicServerException catching procedure

--- Middleware/ExceptionsHandlingMiddleware.cs
+++ Middleware/ExceptionsHandlingMiddleware.cs
@@ -6,27 +6,42 @@ using Microsoft.AspNetCore.Http;
 
 using asp_net_po_schedule_management_server.Dto;
 using asp_net_po_schedule_management_server.Utils;
+using asp_net_po_schedule_management_server.Exceptions;
 
 
 namespace asp_net_po_schedule_management_server.Middleware
 {
     public sealed class ExceptionsHandlingMiddleware : IMiddleware
     {
+        
+        // Middleware przechwytujący wszystkie złapane wyjątki do odpowiednich klauzur catch i wysyłających
+        // do użytkownika obiekt JSON z informacją o błędzie
         public async Task InvokeAsync(HttpContext context, RequestDelegate next)
         {
             try {
                 await next.Invoke(context);
             }
-            catch (Exception err) {
-                int errCode = (int)HttpStatusCode.InternalServerError;
-                context.Response.StatusCode = errCode;
-                await context.Response.WriteAsync(new ServerExceptionResponseDto()
-                {
-                    ExceptionMessage = err.Message,
-                    ExceptionErrorCode = errCode,
-                    ServerTimestamp = ApplicationUtils.GetTimestamp(DateTime.Now)
-                }.ToString());
+            catch (BasicServerException err) {
+                int statusCode = (int)err.HttpStatusCode;
+                context.Response.StatusCode = statusCode;
+                await context.Response.WriteAsync(ResponseJsonValue(statusCode, err.Message));
             }
+            catch (Exception err) {
+                int statusCode = (int)HttpStatusCode.InternalServerError;
+                context.Response.StatusCode = statusCode;
+                await context.Response.WriteAsync(ResponseJsonValue(statusCode, err.Message));
+            }
+        }
+
+        
+        private string ResponseJsonValue(int statusCode, string message)
+        {
+            return new ServerExceptionResponseDto()
+            {
+                ExceptionMessage = message,
+                ExceptionErrorCode = statusCode,
+                ServerTimestamp = ApplicationUtils.GetTimestamp(DateTime.Now)
+            }.ToString();
         }
     }
 }
