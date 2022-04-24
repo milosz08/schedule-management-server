16a8b928105e00417b9202e96c1d45580e64e584
Miłosz Gilga <gilgamilosz451@gmail.com>
2022-04-25 01:34:08 +0200

    create ApiEndpoints helper class

---
+++ Utils/ApiEndpoints.cs
@@ -0,0 +1,15 @@
+﻿namespace asp_net_po_schedule_management_server.Utils
+{
+    public static class ApiEndpoints
+    {
+        // kontroler AuthController
+        public const string LOGIN = "login";
+        public const string REGISTER = "register";
+        public const string CHANGE_PASSWORD = "change-password";
+        public const string REFRESH_TOKEN = "refresh-token";
+
+        // kontroller FileController
+        public const string GET_AVATAR = "get-avatar";
+        public const string ADD_AVATAR = "add-avatar";
+    }
+}
