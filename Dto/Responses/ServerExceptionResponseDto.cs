6ef595ea20f960fcca8e7c426f20979443413a77
Miłosz Gilga <gilgamilosz451@gmail.com>
2022-04-25 01:43:52 +0200

    refactor dtos directories

--- Dto/Responses/ServerExceptionResponseDto.cs
+++ Dto/Responses/ServerExceptionResponseDto.cs
@@ -3,7 +3,7 @@
 using System.Text.Json;
 
 
-namespace asp_net_po_schedule_management_server.Dto.Misc
+namespace asp_net_po_schedule_management_server.Dto.Responses
 {
     public sealed class ServerExceptionResponseDto
     {
--- Dto/Shared/DictionaryHashInjectionFragment.cs
+++ Dto/Shared/DictionaryHashInjectionFragment.cs
@@ -1,6 +1,6 @@
 ﻿namespace asp_net_po_schedule_management_server.Dto.Shared
 {
-    public abstract class ResponseDictionaryHashInjection
+    public abstract class DictionaryHashInjectionFragment
     {
         public string DictionaryHash { get; set; }
     }
