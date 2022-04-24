068f00a9b8e9d0d7350bb579685fe7d23c02b9cb
Miłosz Gilga <gilgamilosz451@gmail.com>
2022-04-25 01:42:47 +0200

    create Refresh Token DTO object

---
+++ Dto/CrossQuery/RefreshTokenDto.cs
@@ -0,0 +1,9 @@
+﻿namespace asp_net_po_schedule_management_server.Dto.CrossQuery
+{
+    public sealed class RefreshTokenDto
+    {
+        public string BearerToken { get; set; }
+        
+        public string RefreshBearerToken { get; set; }
+    }
+}
