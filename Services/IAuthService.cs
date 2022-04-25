4308850ea7c893e49050020baf71b45dd0c40433
Miłosz Gilga <gilgamilosz451@gmail.com>
2022-04-25 02:02:06 +0200

    create and implement refresh token service handler (with generating refresh token in login service handler)

--- Services/IAuthService.cs
+++ Services/IAuthService.cs
@@ -1,7 +1,9 @@
 ﻿using System.Threading.Tasks;
 
 using asp_net_po_schedule_management_server.Dto.AuthDtos;
-using asp_net_po_schedule_management_server.Dto.Misc;
+using asp_net_po_schedule_management_server.Dto.Requests;
+using asp_net_po_schedule_management_server.Dto.Responses;
+using asp_net_po_schedule_management_server.Dto.CrossQuery;
 
 
 namespace asp_net_po_schedule_management_server.Services
@@ -9,6 +11,7 @@ namespace asp_net_po_schedule_management_server.Services
     public interface IAuthService
     {
         Task<LoginResponseDto> UserLogin(LoginRequestDto user);
+        Task<RefreshTokenDto> UserRefreshToken(RefreshTokenDto dto);
         Task<RegisterNewUserResponseDto> UserRegister(RegisterNewUserRequestDto user);
         Task<PseudoNoContentResponseDto> UserChangePassword(ChangePasswordRequestDto dto, string userId);
     }
