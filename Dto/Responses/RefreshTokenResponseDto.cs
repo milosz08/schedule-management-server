e71a50452148a13d830029725354e98607c71b35
Miłosz Gilga <gilgamilosz451@gmail.com>
2022-04-27 23:23:59 +0200

    add TokenExpirationDate element into response refresh token DTO

---
+++ Dto/Responses/RefreshTokenResponseDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace asp_net_po_schedule_management_server.Dto.Responses
+{
+    public sealed class RefreshTokenResponseDto
+    {
+        public string BearerToken { get; set; }
+        
+        public string RefreshBearerToken { get; set; }
+        
+        public DateTime TokenExpirationDate { get; set; }
+    }
+}
