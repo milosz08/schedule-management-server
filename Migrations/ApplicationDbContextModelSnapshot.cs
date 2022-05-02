979fe7c3cbe99907e96301d75d1a0803b014122a
Miłosz Gilga <gilgamilosz451@gmail.com>
2022-05-02 05:28:08 +0200

    update auto-updatable project files

--- Migrations/ApplicationDbContextModelSnapshot.cs
+++ Migrations/ApplicationDbContextModelSnapshot.cs
@@ -132,6 +132,46 @@ namespace asp_net_po_schedule_management_server.Migrations
                     b.ToTable("jwt-tokens");
                 });
 
+            modelBuilder.Entity("asp_net_po_schedule_management_server.Entities.ResetPasswordOtp", b =>
+                {
+                    b.Property<long>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("bigint")
+                        .HasColumnName("primary-key");
+
+                    b.Property<DateTime>("CreatedDate")
+                        .HasColumnType("datetime(6)")
+                        .HasColumnName("created-date");
+
+                    b.Property<string>("Email")
+                        .IsRequired()
+                        .HasColumnType("longtext")
+                        .HasColumnName("user-email");
+
+                    b.Property<string>("Otp")
+                        .IsRequired()
+                        .HasColumnType("longtext")
+                        .HasColumnName("user-otp");
+
+                    b.Property<DateTime>("OtpExpired")
+                        .HasColumnType("datetime(6)")
+                        .HasColumnName("otp-expired");
+
+                    b.Property<long>("PersonId")
+                        .HasColumnType("bigint")
+                        .HasColumnName("person-key");
+
+                    b.Property<DateTime>("UpdatedDate")
+                        .HasColumnType("datetime(6)")
+                        .HasColumnName("updated-date");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("PersonId");
+
+                    b.ToTable("reset-password-otp");
+                });
+
             modelBuilder.Entity("asp_net_po_schedule_management_server.Entities.Role", b =>
                 {
                     b.Property<long>("Id")
@@ -174,6 +214,17 @@ namespace asp_net_po_schedule_management_server.Migrations
                 });
 
             modelBuilder.Entity("asp_net_po_schedule_management_server.Entities.RefreshToken", b =>
+                {
+                    b.HasOne("asp_net_po_schedule_management_server.Entities.Person", "Person")
+                        .WithMany()
+                        .HasForeignKey("PersonId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.Navigation("Person");
+                });
+
+            modelBuilder.Entity("asp_net_po_schedule_management_server.Entities.ResetPasswordOtp", b =>
                 {
                     b.HasOne("asp_net_po_schedule_management_server.Entities.Person", "Person")
                         .WithMany()
