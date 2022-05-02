79bffd3c8cfd691e4da486e50a77e095c8a2f641
Miłosz Gilga <gilgamilosz451@gmail.com>
2022-05-02 05:35:40 +0200

    add custom regexp password validator

--- Dto/Requests/ChangePasswordRequestDto.cs
+++ Dto/Requests/ChangePasswordRequestDto.cs
@@ -9,6 +9,10 @@ namespace asp_net_po_schedule_management_server.Dto.Requests
         public string OldPassword { get; set; }
         
         [Required(ErrorMessage = "Pole nowego hasła nie może być puste")]
+        [RegularExpression(
+            @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,15}$",
+            ErrorMessage = "Hasło musi mieć minimum 8 znaków, zawierać co najmniej jedną liczbę, " +
+                           "jedną wielką literę oraz jeden znak specjalny.")]
         public string NewPassword { get; set; }
         
         [Required(ErrorMessage = "Pole potwierdzenia nowego hasła nie może być puste")]
