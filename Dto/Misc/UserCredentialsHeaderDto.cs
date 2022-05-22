fba17987f1f58d6bf7491b6f280da45027eabb2a
Miłosz Gilga <gilgamilosz451@gmail.com>
2022-05-22 11:20:43 +0200

    remove unused files

--- Dto/Misc/UserCredentialsHeaderDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace asp_net_po_schedule_management_server.Dto.Misc
-{
-    public class UserCredentialsHeaderDto
-    {
-        public string Login { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
