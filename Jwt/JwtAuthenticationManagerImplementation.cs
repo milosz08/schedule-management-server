cf900b46ae350d8c81c0c9dbe6a6735df41f34e9
Miłosz Gilga <gilgamilosz451@gmail.com>
2022-07-06 05:32:46 +0200

    insert copyright header comment content (source and template files)

--- Jwt/JwtAuthenticationManagerImplementation.cs
+++ Jwt/JwtAuthenticationManagerImplementation.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿/*
+ * Copyright (c) 2022 by MILOSZ GILGA <https://miloszgilga.pl> <https://github.com/Milosz08>
+ * Silesian University of Technology | Politechnika Śląska
+ *
+ * File name | Nazwa pliku: JwtAuthenticationManagerImplementation.cs
+ * Project name | Nazwa Projektu: asp-net-po-schedule-management-server
+ *
+ * Klient | Client: <https://github.com/Milosz08/Angular_PO_Schedule_Management_Client>
+ * Serwer | Server: <https://github.com/Milosz08/ASP.NET_PO_Schedule_Management_Server>
+ *
+ * RestAPI for the Angular application to manage schedule for sample university. Written with the ASP.NET Core
+ * and Entity Framework with mySQL database. Project for the teaching course "Objected Oriented Programming".
+ *
+ * RestAPI dla aplikacji Angular do zarządzania planem zajęć przykładowej uczelni wyższej. Napisane w oparciu o
+ * ASP.NET Core oraz Entity Framework z bazą danych mySQL. Projekt wykonany na zajęcia "Programowanie Obiektowe".
+ */
+
+using System;
 using System.Text;
 using System.Security.Claims;
 using System.Security.Cryptography;
